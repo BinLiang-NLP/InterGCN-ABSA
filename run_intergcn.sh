d9d971e705a9d9fd97474839875f7d96a97206ce
Akuchi <bin.liang@stu.hit.edu.cn>
2020-10-28 14:24:08 +0800

    Update run_intergcn.sh

--- run_intergcn.sh
+++ run_intergcn.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+#########################################################################
+# Author: Bin Liang
+# mail: bin.liang@stu.hit.edu.cn
+#########################################################################
+
 CUDA_VISIBLE_DEVICES=1 python3 train.py --model_name intergcn --dataset lap14 --save True --learning_rate 1e-3 --seed 29 --batch_size 16
