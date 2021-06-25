2a900948d9b6d96d1bccfa739aeaeb19b783f172
Akuchi <bin.liang@stu.hit.edu.cn>
2021-06-25 18:01:55 +0800

    Update generate_graph_for_aspect.py

--- generate_graph_for_aspect.py
+++ generate_graph_for_aspect.py
@@ -34,7 +34,7 @@ def process(filename):
     lines = fin.readlines()
     fin.close()
     idx2graph = {}
-    fout = open(filename+'.graph_a', 'wb')
+    fout = open(filename+'.graph_af', 'wb')
     graph_idx = 0
     for i in range(len(lines)):
         aspects, polarities, positions, text = lines[i].split('\t')
