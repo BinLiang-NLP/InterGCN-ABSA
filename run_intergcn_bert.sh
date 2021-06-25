603b0bd11f96adee13562053981bf97b3f7a5b0f
Akuchi <bin.liang@stu.hit.edu.cn>
2021-06-25 17:32:40 +0800

    add *.sh

---
+++ run_intergcn_bert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#########################################################################
+# Author: Bin Liang
+# mail: bin.liang@stu.hit.edu.cn
+#########################################################################
+
+CUDA_VISIBLE_DEVICES=2 python3 train_bert.py --model_name intergcn_bert --dataset rest15 --num_epoch 20 --lr 5e-5 --batch_size 16 --seed 68
