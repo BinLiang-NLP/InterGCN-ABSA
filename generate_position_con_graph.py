c8e0a7305b40d6e703e45186eaa40d155ef9d475
Akuchi <bin.liang@stu.hit.edu.cn>
2021-06-25 18:02:29 +0800

    Update generate_position_con_graph.py

--- generate_position_con_graph.py
+++ generate_position_con_graph.py
@@ -63,7 +63,7 @@ def process(filename):
     lines = fin.readlines()
     fin.close()
     idx2graph = {}
-    fout = open(filename+'.graph_con_a', 'wb')
+    fout = open(filename+'.graph_inter', 'wb')
     graph_idx = 0
     for i in range(len(lines)):
         aspects, polarities, positions, text = lines[i].split('\t')
